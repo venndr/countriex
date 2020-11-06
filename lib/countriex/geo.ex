799994960dcf5c3bb6e0dcfbe95ef70a68202210
Piotr Szmielew <p.szmielew@ava.waw.pl>
2020-11-06 17:53:55 +0100

    Add typespecs

--- lib/countriex/geo.ex
+++ lib/countriex/geo.ex
@@ -1,3 +1,23 @@
 defmodule Countriex.Geo do
-  defstruct [:latitude, :latitude_dec, :longitude, :longitude_dec, :max_latitude, :max_longitude, :min_latitude, :min_longitude]
+  @type t :: %__MODULE__{
+          latitude: float(),
+          latitude_dec: float(),
+          longitude: float(),
+          longitude_dec: float(),
+          max_latitude: float(),
+          max_longitude: float(),
+          min_latitude: float(),
+          min_longitude: float()
+        }
+
+  defstruct [
+    :latitude,
+    :latitude_dec,
+    :longitude,
+    :longitude_dec,
+    :max_latitude,
+    :max_longitude,
+    :min_latitude,
+    :min_longitude
+  ]
 end
